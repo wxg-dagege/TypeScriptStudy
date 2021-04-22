3a316d351eebd8d1000146e0dcf3f91a360054a8
wusheng <wxgdagege@gmail.com>
2021-04-22 11:10:19 +0800

    增加了两个插件HtmlWebpackPlugin vue-loader vue-template-compiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
+    output: {
+        filename: '[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
     mode: 'development',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader'
+            },
+            {
+                test: /\.vue?$/,
+                use: 'vue-loader'
             }
         ]
     },
     devtool: "source-map",
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        compress: true,
+        compress: true, //为每个静态文件提供压缩
         port: 9001
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
-    output: {
-        filename: '[hash].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    }
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "管理输出"
+        })
+    ]
 }
